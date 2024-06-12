88e9eb42e7082b7a2dec329af3f95026a77b893e
wenqinglim <lim_wenq@hotmail.com>
2024-06-12 22:55:12 +0100

    Tidy agent code

--- agents/qna_agent.py
+++ agents/qna_agent.py
@@ -25,10 +25,6 @@ async def call_gpt4(ctx: Context):
 	
     client = gf.create_client()
     assistant = gf.create_assistant(client, name, assistant_desc, 'gpt-4o')
-    # # TODO: Take in individual member's information as input (json format)
-    # indiv_info = indiv_input
-
-    # prompt = f"Given the following individual's information: {indiv_info}, {question}"
 
     json_output_format = """
      {"question": "What is the frequency and mode of pension payment for the member?", "relevant_sections": ["Section 1.1 (Frequency Of Payment)",
@@ -43,20 +39,7 @@ async def call_gpt4(ctx: Context):
     response, citations = gf.prompt_gpt4(client, assistant, prompt)
     print(response)
     print(citations)
-	
-# Send delta calories to grocery agent
 
-# RECIPIENT_ADDRESS = (
-# 	"groceryagent://agent1qt27mhu8js84x7zh30sxegf0m5va2gxtk3sqns4ptvrutzv8l0kuke9qq43"
-# )
-
-# @gpt4agent.on_interval(period=60.0)
-# async def send_message(ctx: Context):
-# 	# ctx.logger.info(f"Sending message to {RECIPIENT_ADDRESS}")
-# 	# await ctx.send(RECIPIENT_ADDRESS, Message(message="Hello there testagent."))
-	
-# 	delta = ctx.storage.get("delta")
-# 	await ctx.send(RECIPIENT_ADDRESS, Message(message=str(delta)))
 
 if __name__ == "__main__":
 	gpt4agent.run()
